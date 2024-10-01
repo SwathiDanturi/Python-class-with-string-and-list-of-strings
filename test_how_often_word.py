7300901ac2ffc8cb35ffb62ee8fba0691b1fc231
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-30 21:10:55 -0400

    create new test modules and write docstrings

---
+++ test_how_often_word.py
@@ -0,0 +1,7 @@
+"""
+Test Notes instance method how_often_word()
+File: test_how_often_word.py
+Developer: Swathi Danturi
+Collaborator(s): enter your collaborator(s) name(s): None
+Date: 09/30/2024
+"""
---
+++ test_word_histogram.py
@@ -0,0 +1,7 @@
+"""
+Test Notes instance method word_histogram()
+File: test_word_histogram.py
+Developer: Swathi Danturi
+Collaborator(s): enter your collaborator(s) name(s): None
+Date: 09/30/2024
+"""
